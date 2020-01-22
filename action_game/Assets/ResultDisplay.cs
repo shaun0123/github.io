d27afe19f1f9c86633f37c9839974d51a5049f59
shaun0123 <shion.tominaga@litalico.co.jp>
2020-01-22 16:53:18 +0900

    CUで作ったアクションゲーム

---
+++ action_game/Assets/ResultDisplay.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultDisplay : MonoBehaviour
+{
+    public GameObject ClearText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ClearText.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionStay(Collision hit)
+    {
+        if (hit.gameObject.tag == "Player")
+        {
+            // ARRANGE:ゴールに触れた時の処理を書く
+            ClearText.SetActive(true);
+        }
+
+    }
+}
